b5785a2ebfdd43ba63e998be343c1f25a1609b63
chukwuebuka emmanuel ani <chukwuebukaemmanuelani@gmail.com>
2023-06-21 06:55:32 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,22 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- */
+ * jack_bauer- prints every minute of the day
+*/
+
 void jack_bauer(void)
 {
-	int i, j;
+	int min, hr;
 
-	for (i = 0; i < 24; i++)
+	for (hr = 0; hr <= 23; ++hr)
 	{
-		for (j = 0; j < 60; j++)
+		for (min = 0; min <= 59; ++min)
 		{
-			_putchar(i / 10 + 48);
-			_putchar(i % 10 + 48);
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
-			_putchar(j / 10 + 48);
-			_putchar(j % 10 + 48);
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
